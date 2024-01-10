dac675b6087ff8348667a9d15d7efb0bac090e00
mizan <mizanurrahaman551@gmail.com>
2024-01-10 13:14:10 +0600

    first commit

---
+++ src/script.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    var x = document.querySelector(".jsDropdown");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
+function myFunction2() {
+    var x = document.querySelector(".jsDropdown2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
+function toggleModal() {
+    var x = document.querySelector(".jsDropdown3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
+function toggleModal4() {
+    var x = document.querySelector(".jsDropdown4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
+function toggleModal5() {
+    var x = document.querySelector(".jsDropdown5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
